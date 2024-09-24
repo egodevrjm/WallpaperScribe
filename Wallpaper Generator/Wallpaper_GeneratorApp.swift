a7588f6ccc926760a2eebf994916c3e57ec5741f
Ryan Morrison <ryanmorrison@gmail.com>
2024-09-24 23:32:22 +0100

    Initial Commit

---
+++ Wallpaper Generator/Wallpaper_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wallpaper_GeneratorApp.swift
+//  Wallpaper Generator
+//
+//  Created by Ryan Morrison on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Wallpaper_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
